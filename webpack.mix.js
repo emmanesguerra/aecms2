b1207ec1a39952b0c09bd5a0bf0d578f928c6a46
emmanesguerra <emman.esguerra2013@gmail.com>
2020-06-26 13:44:57 +0800

    change path for compiled admin css and js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('core/Layouts/assets/js/app.js', 'public/js/admin')
+    .sass('core/Layouts/assets/sass/app.scss', 'public/css/admin');

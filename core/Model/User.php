21cede3200c63e9e423db4a4fa88c7eba58a794b
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-27 12:33:11 +0800

    Setup User for roles and permission application

--- core/Model/User.php
+++ core/Model/User.php
@@ -5,10 +5,42 @@ namespace Core\Model;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
 {
-    //
-    protected $guarded = ['email_verified_at', 'remember_token'];
+    use Notifiable;
+    use HasRoles;
+  
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstname', 'middlename', 'lastname', 'email', 'password', 'usertype_id', 'created_by', 'updated_by', 'created_at', 'updated_at'
+    ];
+  
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+  
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
     
     public function setPasswordAttribute($pass){
         $this->attributes['password'] = Hash::make($pass);

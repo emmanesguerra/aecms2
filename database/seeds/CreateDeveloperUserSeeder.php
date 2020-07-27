0a6b911f2af28aab7f614757d511a58614840fe1
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-27 13:05:45 +0800

    Create initial user and role using seeder

---
+++ database/seeds/CreateDeveloperUserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Core\Model\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class CreateDeveloperUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::unsetEventDispatcher();
+        $user = User::create([
+        	'firstname' => 'Emmanuelle', 
+        	'middlename' => 'Magtibay', 
+        	'lastname' => 'Esguerra', 
+        	'email' => 'admin@gmail.com',
+        	'password' => Illuminate\Support\Facades\Hash::make('namme'),
+        	'usertype_id' => 1,
+        	'created_by' => '1'
+        ]);
+  
+        $role = Role::create(['name' => 'Ae Developer']);
+   
+        $permissions = Permission::pluck('id','id')->all();
+  
+        $role->syncPermissions($permissions);
+   
+        $user->assignRole([$role->id]);
+    }
+}

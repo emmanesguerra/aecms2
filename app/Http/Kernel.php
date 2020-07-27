630704b5cf5955794076c263ce393edcfdac99e1
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-27 12:33:44 +0800

    update Kernel for route middleware of roles and permissions

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,10 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        /*
+         * Roles and Permissions
+         */
+	'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+	'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
     ];
 }

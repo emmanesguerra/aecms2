514c9315888199e6debb6902f35e5d72d75196b7
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-27 11:21:32 +0800

    add mutator to Password field; add guarded attri for email_verified and remember_token

--- core/Model/User.php
+++ core/Model/User.php
@@ -2,9 +2,15 @@
 
 namespace Core\Model;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
     //
+    protected $guarded = ['email_verified_at', 'remember_token'];
+    
+    public function setPasswordAttribute($pass){
+        $this->attributes['password'] = Hash::make($pass);
+    }
 }

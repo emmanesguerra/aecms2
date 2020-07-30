07205e766c649dad7e3467c19d14df862d02b6bc
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-30 10:09:53 +0800

    re arrange running of seeder classes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
          $this->call([
              PermissionTableSeeder::class,
-             CreateDeveloperUserSeeder::class,
              MolduleSeeder::class,
+             CreateDeveloperUserSeeder::class,
          ]);
     }
 }

a338427ebb731e696bd3b5f2f9617693a54e97db
emmanesguerra <emman.esguerra2013@gmail.com>
2020-07-30 10:14:09 +0800

    initial commit of Content module class files and migration file

---
+++ core/Model/Content.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Core\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model
+{
+    //
+}
---
+++ database/migrations/2020_07_30_015822_create_contents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->text('html_template')->nullable();
+            $table->char('type', 1);
+            $table->string('class_namespace', 250)->nullable();
+            $table->string('method_name', 50)->nullable();
+            $table->unsignedInteger('created_by');
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}

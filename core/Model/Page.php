15c0b0e6da66e56154a43218571e0e91295ffec6
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-04 09:44:15 +0800

    add contents relationship

--- core/Model/Page.php
+++ core/Model/Page.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Page extends Model
 {
     protected $fillable = ['url', 'title', 'description', 'javascripts', 'css', 'template', 'template_html'];
+    
+    /**
+     * A page may be given various contents.
+     */
+    public function contents()
+    {
+        return $this->belongsToMany(
+                'Core\Model\Content',
+                'page_has_contents',
+                'page_id',
+                'content_id'
+        );
+    }
 }

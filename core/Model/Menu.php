b1176e95e8359545f6e376c2e7a525ed77e9e62c
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-05 10:10:14 +0800

    initial commit of menu model

---
+++ core/Model/Menu.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Core\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+  
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'parent_id', 'page_id', 'lft', 'rgt', 'lvl', 'created_by', 'created_at', 'updated_at'
+    ];
+}

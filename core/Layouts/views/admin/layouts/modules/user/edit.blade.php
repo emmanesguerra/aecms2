631f660530d0463d1a1cfac4e21ff8a2329dfe50
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-06 11:11:15 +0800

    remove confirm modal

--- core/Layouts/views/admin/layouts/modules/user/edit.blade.php
+++ core/Layouts/views/admin/layouts/modules/user/edit.blade.php
@@ -79,25 +79,6 @@
                         <div class="col-sm-12">
                             <input  type="submit" class="btn btn-primary" value="Submit" />
                         </div>
-                        <div class="modal fade" id="settingsModal" tabindex="-1" role="dialog" aria-labelledby="settingsModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="settingsModalLabel">Continue Posting</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Continue saving records?
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <input  type="submit" class="btn btn-primary" value="Save changes" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 {!! Form::close() !!}
             </div>

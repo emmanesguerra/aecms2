40b88eb3051115134988b7a6f833d2602f7cfb66
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-06 21:38:38 +0800

    update app, include intervention/image package

--- config/app.php
+++ config/app.php
@@ -180,6 +180,11 @@ return [
          */
 	Spatie\Permission\PermissionServiceProvider::class,
 	Collective\Html\HtmlServiceProvider::class,
+        
+        /*
+         * Image Providers
+         */
+        Intervention\Image\ImageServiceProvider::class
 
     ],
 
@@ -237,6 +242,10 @@ return [
          */
 	'Form' => Collective\Html\FormFacade::class,
 	'Html' => Collective\Html\HtmlFacade::class,
+        /*
+         * Images
+         */
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];

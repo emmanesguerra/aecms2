e5e5ae10d51460f7c1b8c49db81074120fba756f
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-06 11:12:03 +0800

    add input validation to setting form

--- core/Layouts/views/admin/layouts/modules/setting/form.blade.php
+++ core/Layouts/views/admin/layouts/modules/setting/form.blade.php
@@ -48,21 +48,21 @@
                                 <div class="form-row">
                                     <div  class="custom-control form-group  col-sm-12">
                                         <label class="@error('domain_name') text-danger @enderror" for="domain_name">Domain name *</label>
-                                        <input type="text" class="form-control ae-input-field @error('domain_name') is-invalid @enderror " name="domain_name" value="{{ old('domain_name', $data['model']['domain_name']) }}" id="domain_name" placeholder="ex. your.website.com">
+                                        <input maxlength="191" type="text" class="form-control ae-input-field @error('domain_name') is-invalid @enderror " name="domain_name" value="{{ old('domain_name', $data['model']['domain_name']) }}" id="domain_name" placeholder="ex. your.website.com" required/>
                                         @error('domain_name') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('website_name') text-danger @enderror" for="website_name">Website name *</label>
-                                        <input type="text" class="form-control ae-input-field @error('website_name') is-invalid @enderror " name="website_name" value="{{ old('website_name', $data['model']['website_name']) }}" id="website_name" placeholder="ex. Your Website">
+                                        <input maxlength="191" type="text" class="form-control ae-input-field @error('website_name') is-invalid @enderror " name="website_name" value="{{ old('website_name', $data['model']['website_name']) }}" id="website_name" placeholder="ex. Your Website" required/>
                                         @error('website_name') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('owner') text-danger @enderror" for="owner">Owner</label>
-                                        <input type="text" class="form-control ae-input-field @error('owner') is-invalid @enderror " name="owner" value="{{ old('owner', $data['model']['owner']) }}" id="owner" placeholder="Owner's name">
+                                        <input maxlength="191" type="text" class="form-control ae-input-field @error('owner') is-invalid @enderror " name="owner" value="{{ old('owner', $data['model']['owner']) }}" id="owner" placeholder="Owner's name" />
                                         @error('owner') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
@@ -72,21 +72,21 @@
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('meta_title') text-danger @enderror" for="meta_title">Default Page Title *</label>
-                                        <input type="text" class="form-control ae-input-field @error('meta_title') is-invalid @enderror " name="meta_title" value="{{ old('meta_title', $data['model']['meta_title']) }}" id="meta_title" placeholder="Default page title">
+                                        <input maxlength="191" type="text" class="form-control ae-input-field @error('meta_title') is-invalid @enderror " name="meta_title" value="{{ old('meta_title', $data['model']['meta_title']) }}" id="meta_title" placeholder="Default page title" required/>
                                         @error('meta_title') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('meta_keywords') text-danger @enderror" for="meta_keywords">Keywords/Tags</label>
-                                        <input type="text" class="form-control ae-input-field @error('meta_keywords') is-invalid @enderror " name="meta_keywords" value="{{ old('meta_keywords', $data['model']['meta_keywords']) }}" id="meta_keywords" placeholder="Default page keywords">
+                                        <input type="text" class="form-control ae-input-field @error('meta_keywords') is-invalid @enderror " name="meta_keywords" value="{{ old('meta_keywords', $data['model']['meta_keywords']) }}" id="meta_keywords" placeholder="Default page keywords" />
                                         @error('meta_keywords') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('meta_description') text-danger @enderror" for="meta_description">Page Description *</label>
-                                        <textarea  class="form-control ae-input-field @error('meta_description') is-invalid @enderror " name="meta_description" id="meta_description" placeholder="Default page description" rows="5">{{ old('meta_description', $data['model']['meta_description']) }}</textarea>
+                                        <textarea  class="form-control ae-input-field @error('meta_description') is-invalid @enderror " name="meta_description" id="meta_description" placeholder="Default page description" rows="5" required>{{ old('meta_description', $data['model']['meta_description']) }}</textarea>
                                         @error('meta_description') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
@@ -101,21 +101,21 @@
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('email_recipients') text-danger @enderror" for="email_recipients">Recipients</label>
-                                        <input type="text" class="form-control ae-input-field @error('email_recipients') is-invalid @enderror " name="email_recipients" value="{{ old('email_recipients', $data['model']['email_recipients']) }}" id="email_recipients" placeholder="Recipient">
+                                        <input type="text" class="form-control ae-input-field @error('email_recipients') is-invalid @enderror " name="email_recipients" value="{{ old('email_recipients', $data['model']['email_recipients']) }}" id="email_recipients" placeholder="Recipient" />
                                         @error('email_recipients') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('email_cc') text-danger @enderror" for="email_cc">CCs</label>
-                                        <input type="text" class="form-control ae-input-field @error('email_cc') is-invalid @enderror " name="email_cc" value="{{ old('email_cc', $data['model']['email_cc']) }}" id="email_cc" placeholder="Cc">
+                                        <input type="text" class="form-control ae-input-field @error('email_cc') is-invalid @enderror " name="email_cc" value="{{ old('email_cc', $data['model']['email_cc']) }}" id="email_cc" placeholder="Cc" />
                                         @error('email_cc') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('email_bcc') text-danger @enderror" for="email_bcc">BCCs</label>
-                                        <input type="text" class="form-control ae-input-field @error('email_bcc') is-invalid @enderror " name="email_bcc" value="{{ old('email_bcc', $data['model']['email_bcc']) }}" id="email_bcc" placeholder="Bcc">
+                                        <input type="text" class="form-control ae-input-field @error('email_bcc') is-invalid @enderror " name="email_bcc" value="{{ old('email_bcc', $data['model']['email_bcc']) }}" id="email_bcc" placeholder="Bcc" />
                                         @error('email_bcc') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
@@ -125,7 +125,7 @@
                                 <div class="form-row">
                                     <div  class="form-group  col-sm-12">
                                         <label class="@error('developer') text-danger @enderror" for="developer">Developer</label>
-                                        <input type="text" class="form-control ae-input-field @error('developer') is-invalid @enderror " name="developer" value="{{ old('developer', $data['model']['developer']) }}" id="developer" placeholder="Developer's name">
+                                        <input maxlength="191" type="text" class="form-control ae-input-field @error('developer') is-invalid @enderror " name="developer" value="{{ old('developer', $data['model']['developer']) }}" id="developer" placeholder="Developer's name" />
                                         @error('developer') <div class="invalid-feedback">{{ $message }}</div> @enderror 
                                     </div>
                                 </div>
@@ -148,27 +148,7 @@
                             </div>
                         </div>
                         <div class="col-sm-12 col-md-6 col-lg-6">
-                            <button data-toggle="modal" data-target="#settingsModal" type="button" class="btn btn-primary">Submit</button>
-                            <button type="button" class="btn btn-secondary">Clear</button>
-                        </div>
-                        <div class="modal fade" id="settingsModal" tabindex="-1" role="dialog" aria-labelledby="settingsModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="settingsModalLabel">Continue Posting</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Continue saving records?
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <input  type="submit" class="btn btn-primary" value="Save changes" />
-                                    </div>
-                                </div>
-                            </div>
+                            <input  type="submit" class="btn btn-primary" value="Submit" />
                         </div>
                     </div>
                 </form>

5ee4aae3602d3da0bd8de92c7f35c59fc9eeefb5
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-11 11:23:07 +0800

    fix adding of audit plugin to office module

--- core/Model/Office.php
+++ core/Model/Office.php
@@ -3,9 +3,12 @@
 namespace Core\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Office extends Model
+class Office extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+    
     protected $fillable = ['address', 'contact_person', 'telephone', 'mobile', 'email', 'marker', 'm_width', 'm_height', 'store_hours'];
 
     protected function serializeDate(\DateTimeInterface $date)

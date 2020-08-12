05b042912d63e820de902c33b33da95d28e221ba
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-12 12:57:54 +0800

    add feedback on login template

--- core/Layouts/views/auth/login.blade.php
+++ core/Layouts/views/auth/login.blade.php
@@ -11,6 +11,12 @@
                     <form method="POST" action="{{ route('admin.login') }}">
                         @csrf
 
+                        @if (session('feedback'))
+                        <div class="alert alert-danger text-left">
+                            {{ session('feedback') }}
+                        </div>
+                        @endif
+                        
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 

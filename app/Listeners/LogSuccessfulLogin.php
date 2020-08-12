6de0c057f45739e82d2e8a8945029ee8c3a37bd5
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-12 12:56:46 +0800

    fix adding event listeners for user login and logout

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Core\Model\UserLog;
+
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Login  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        try
+        {
+            DB::beginTransaction();
+            
+            $user = $event->user;
+
+            $userlog = UserLog::create(['log_in' => \Carbon\Carbon::now(), 'ip_address' => $this->request->ip()]);
+
+            if($userlog) {
+                $user->current_ul_id = $userlog->id;
+                $user->save();
+            }
+            
+            DB::commit();
+        } catch (\Exception $ex) {
+            DB::rollback();
+            Log::error($ex->getMessage());
+        }
+    }
+}

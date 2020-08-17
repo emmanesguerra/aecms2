077ff927bb662bae42407f99e279e04a6d663e35
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-17 16:41:22 +0800

    move observers to Core

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,29 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
-use Core\Model\SystemConfig;
-use Core\Observers\SystemConfigObserver;
-
-use Core\Model\User;
-use Core\Observers\UserObserver;
-
-use Core\Model\Module;
-use Core\Observers\ModuleObserver;
-
-use Core\Model\Content;
-use Core\Observers\ContentObserver;
-
-use Core\Model\Page;
-use Core\Observers\PageObserver;
-
-use Core\Model\Menu;
-use Core\Observers\MenuObserver;
-
-use Core\Model\Office;
-use Core\Observers\OfficeObserver;
-
-use Core\Model\MenuSetting;
-use Core\Observers\MenuSettingObserver;
+use Core\Observers\BaseObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -51,13 +29,7 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
         
-        SystemConfig::observe(SystemConfigObserver::class);
-        User::observe(UserObserver::class);
-        Module::observe(ModuleObserver::class);
-        Content::observe(ContentObserver::class);
-        Page::observe(PageObserver::class);
-        Menu::observe(MenuObserver::class);
-        Office::observe(OfficeObserver::class);
-        MenuSetting::observe(MenuSettingObserver::class);
+        $baseobserver = new BaseObserver();
+        $baseobserver->init();
     }
 }

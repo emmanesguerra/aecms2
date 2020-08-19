5e69e0980d8c64e2dbab7aac1dbc5cd45db9c661
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-19 20:06:20 +0800

    initial commit of tinymce default content

---
+++ resources/views/tinymce-default-content.blade.php
@@ -0,0 +1,77 @@
+<div class="aetinymce-content">
+    <h1>H1. What is Lorem Ipsum?</h1>
+    <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. <a href="https://www.lipsum.com/">Lorem Ipsum</a> has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+    <h2>H2. Why do we use it?</h2>
+    <p><a href="https://www.lipsum.com/">Lorem Ipsum</a> is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using <a href="https://www.lipsum.com/">Lorem Ipsum</a> is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+
+    <h3>Sample display of an Ordered List</h3>
+    <h5>Common Website Contents</h5>
+    <ol>
+        <li>Homepage</li>
+        <li>About Us</li>
+        <li>Services</li>
+        <li>Products</li>
+        <li>News</li>
+        <li>Contact Us</li>
+    </ol>
+
+
+    <h3>Sample display of an Unordered List</h3>
+    <h5>Benefits of a having a website</h5>
+    <ul>
+        <li><a href="https://www.lipsum.com/">Improve Your Advertising Effectiveness</a></li>
+        <li><a href="https://www.lipsum.com/">Save Money on Printing and Distribution Costs</a></li>
+        <li>Easy Access to New Customers</li>
+        <li>Easy to Use and Update</li>
+        <li>Improve Productivity</li>
+        <li>Educate Your Customers Us</li>
+        <li>Expand Your Market</li>
+        <li>Extend Your Local Reach</li>
+        <li>and etc.</li>
+    </ul>
+
+    <h3>Sample display of a tabular data</h3>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><a href="https://www.lipsum.com/">Alfreds Futterkiste</a></td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+            </tr>
+            <tr>
+                <td><a href="https://www.lipsum.com/">Centro comercial Moctezuma</a></td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+            </tr>
+            <tr>
+                <td><a href="https://www.lipsum.com/">Ernst Handel</a></td>
+                <td>Roland Mendel</td>
+                <td>Austria</td>
+            </tr>
+            <tr>
+                <td>Island Trading</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+            </tr>
+            <tr>
+                <td>Laughing Bacchus Winecellars</td>
+                <td>Yoshi Tannamuri</td>
+                <td>Canada</td>
+            </tr>
+            <tr>
+                <td>Magazzini Alimentari Riuniti</td>
+                <td>Giovanni Rovelli</td>
+                <td>Italy</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
